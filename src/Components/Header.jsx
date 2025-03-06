afa1c03f7091f8b0de72a42813c23d59e5687458
gamscodes <gaminee.ram30@gmail.com>
2025-03-05 22:20:57 -0500

    made changes in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
         <div className='flex justify-between'>
             <div className='flex items-center justify-center gap-2'>
                 <img src="./assets/Logo.svg" alt="logo" style={{ width: "60px", height: "auto" }} />
-                <button>Hosterr is hiring</button>
+                <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hosterr is hiring</button>
             </div>
-            <div>
-                <FaBars />
+            <div className='flex items-center'>
+                <FaBars size={24} />
             </div>
         </div>
     )
